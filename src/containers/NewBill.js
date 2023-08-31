7a27f4da277ae9e8a129714818f3ef9020e12398
Kelian Halleray <kelianhalleray@gmail.com>
2023-08-31 16:20:13 +0200

    Résolution des erreurs rouges , de login et bills.js

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -16,15 +16,30 @@ export default class NewBill {
     new Logout({ document, localStorage, onNavigate })
   }
   handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    e.preventDefault();
+  
+    const allowedFileFormat = ['jpeg', 'jpg', 'png'];
+    const fileInput = document.querySelector(`input[data-testid="file"]`);
+    const file = fileInput.files[0];
+  
+    const filePath = e.target.value.split(/\\/g);
+    const fileName = filePath[filePath.length - 1];
 
+    if (!file) {
+      console.error("No file selected.");
+      return;
+    }
+    if (!allowedFileFormat.includes(fileName.split('.').pop().toLowerCase())) {
+      fileInput.value = "";
+      console.error("Invalid file format. Allowed formats are jpeg, jpg and png");
+      return;
+    }
+  
+    const formData = new FormData();
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    formData.append('file', file);
+    formData.append('email', email);
+  
     this.store
       .bills()
       .create({
@@ -33,13 +48,16 @@ export default class NewBill {
           noContentType: true
         }
       })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+      .then(({ fileUrl, key }) => {
+        console.log(fileUrl);
+        this.billId = key;
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+      })
+      .catch(error => console.error(error));
   }
+  
+
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
@@ -47,9 +65,9 @@ export default class NewBill {
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
@@ -65,12 +83,12 @@ export default class NewBill {
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH['Bills'])
+        })
+        .catch(error => console.error(error))
     }
   }
 }
--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,8 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  const sortedBills = data && data.length ? [...data].sort((a, b) => b.date.localeCompare(a.date)) : [];
+  return sortedBills.map(bill => row(bill)).join("");
 }
 
 export default ({ data: bills, loading, error }) => {
